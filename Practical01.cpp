b04e551d650a6f4ecebcadf6e84aa5021af2316b
Tanujrawat2208 <91190314+Tanujrawat2208@users.noreply.github.com>
2021-12-05 09:11:45 +0530

    Update Practical01.cpp

--- Practical01.cpp
+++ Practical01.cpp
@@ -1,31 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
-	char ch = 'A';
-	
-	int a = 1456000;
-	short b = 95;
-	long c = 123456789;
-	
-	float d = 10.568f;
-	double e = 15556.5556669;
-	long double f = 102568955.4644335L;
-	
-	bool g = true;
-	
-	cout << "Size of Character: \t" << sizeof(char) << "\t" << sizeof(ch) << endl;
-	
-	cout << "Size of Integer: \t" << sizeof(int) << "\t" << sizeof(a) << endl;
-	cout << "Size of Short Integer: \t" << sizeof(short) << "\t" << sizeof(b) << endl;
-	cout << "Size of Long Integer: \t" << sizeof(long) << "\t" << sizeof(c) << endl;
-	
-	cout << "Size of Floating Point: " << sizeof(float) << "\t" << sizeof(d) << endl;
-	cout << "Size of Double: \t" << sizeof(double) << "\t" << sizeof(e) << endl;
-	cout << "Size of Long Double: \t" << sizeof(long double) << "\t" << sizeof(f) << endl;
-	
-	cout << "Size of Boolean: \t" << sizeof(bool) << "\t" << sizeof(g) << endl;
-	
-	return 0;
+    cout<<"Size of Integer is = "<< sizeof(int)<< " bytes \n";
+
+    cout<<"Size of signed Integer is = "<< sizeof(signed int)<< " bytes \n";
+
+    cout<<"Size of unsigned Integer is = "<< sizeof(unsigned int)<< " bytes \n";
+
+    cout<<"Size of character is = "<< sizeof(char)<< " bytes \n";
+    
+    cout<<"Size of character is = "<< sizeof(signed char)<< " bytes \n";
+    
+    cout<<"Size of character is = "<< sizeof(unsigned char)<< " bytes \n";
+    
+    cout<<"Size of float is = "<< sizeof(float)<< " bytes \n";
+    
+    cout<<"Size of double is = "<< sizeof(double)<< " bytes \n";
+   
+   
+    
+    
+    return 0;
+
 }
